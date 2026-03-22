b17fd20783f0486ab8c45bc296425cc3a3e36902
belworf <belworf4.0@gmail.com>
2026-03-22 19:34:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,17 @@ const cacheName = 'libertus-v2';
 const assets = ['./', './index.html', './assets/logo.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
